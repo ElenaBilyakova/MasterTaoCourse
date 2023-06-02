50caa5f32b93dd07a2e6029eafa43d5a75451fe9
Your Name1~ <bilyakovae@ukr.net>
2023-06-02 17:12:47 +0300

    02.06.23

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,7 +1,38 @@
-$( document ).ready(function() {
-    $('.first-screen-slider').slick();
+$(document).ready(function() {
+    $('.first-screen-slider').slick({
+        dots: true,
+        speed: 1000,
+        appendArrows: ".container-arrows",
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+                    // fade: true,
+                }
+            },
 
-    Fancybox.bind('[data-fancybox="gallery"]', {
-        // Your custom options
-      });
+        ]
+    });
+
+    // $('.navbar-toggler').on('click', function(){
+    //   $('.navbar').toggleClass('margin-bottom')
+    // })
+
+
 });
+
+
+function checkboxContainerPadding() {
+    const checkButton2 = document.querySelectorAll('.check-button');
+    
+    checkButton2.forEach(item => {
+        item.closest('[class*="col"]').classList.add('padding-top');
+    }) 
+}
+
+
+if (document.querySelector('.calculation-form')) {
+      checkboxContainerPadding()
+} else {
+    null
+}
